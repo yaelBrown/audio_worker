67a3d1f88e2e0a08924fe6bf9e32f7a6138274f8
yaelBrown <yaelrbrown@gmail.com>
2021-10-11 23:02:25 -0500

    feat: able to read bpm of mp3 files

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import librosa
+import os
 
 app = Flask(__name__)
 
@@ -11,19 +12,25 @@ _check = "617564696f5f776f726b65720a"
 @app.route("/")
 def testApi():
   try: 
-    if request.form["_"] == _check:       
-      audio = lr.ex(request.files["audio"])
-      y, sr = lr.load(audio)
+    if request.form["_"] == _check:  
+      audio = request.files["audio"]
+      fileName, content_type, length = audio.filename, audio.content_type, audio.content_length
+      
+      out = {"fileName": fileName, "content_type": content_type, "length": length}
+      
+      request.files["audio"].save(os.path.join(os.getcwd(), fileName))
+
+      y, sr = librosa.load(fileName, sr=None)
       onset_env = librosa.onset.onset_strength(y, sr=sr)
-      tempo = lr.beat.tempo(onset_envelope=onset_env, sr=sr)
-      print(tempo)
+      tempo = librosa.beat.tempo(onset_envelope=onset_env, sr=sr)
 
-      return "looks good"
-  except Exception:
-      return {"msg": "err"}
+      out["bpm"] = tempo[0]
+
+      return out
+  except Exception as e:
+      print(e)
+      return {"msg": "error"}
   
-
-
 @app.route("/test")
 def testdb():
   return {'msg': 'ok'}
